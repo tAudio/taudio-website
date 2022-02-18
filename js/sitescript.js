332a33586faf4531ab9068df45596aecc721495f
taudio <taudio432@gmail.com>
2022-02-18 19:41:41 +0200

    added site pages

---
+++ js/sitescript.js
@@ -0,0 +1,28 @@
+current_section = "home";
+
+$(function(){
+
+    console.log("good afternon!");
+
+    $('.nav_item').click(function(e){
+        var clicked_nav = e.target.getAttribute('name').slice(3, e.target.getAttribute('name').length);
+        if(clicked_nav == current_section){
+            return;
+        }
+
+        current_section = clicked_nav;
+        console.log("current_section is "+current_section);
+        Render();
+    });
+
+    Render();
+
+  
+  });
+
+  function Render(){
+
+    $('.content_item').hide();
+    $("#"+current_section).show();
+
+  }
