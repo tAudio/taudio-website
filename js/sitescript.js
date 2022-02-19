2edb100d5eb2e3c0387ac41c10caaec71b3195ec
taudio <taudio432@gmail.com>
2022-02-19 18:41:38 +0200

    routing bug fixed

--- js/sitescript.js
+++ js/sitescript.js
@@ -3,6 +3,7 @@ current_section = "home";
 $(function(){
 
     console.log("good afternon!");
+    history.pushState({'pageState': current_section}, current_section);
 
     $('.nav_item').click(function(e){
         var clicked_nav = e.target.getAttribute('name').slice(3, e.target.getAttribute('name').length);
@@ -13,21 +14,17 @@ $(function(){
         current_section = clicked_nav;
         console.log("current_section is "+current_section);
         $("#chk1").prop('checked', false);
+        history.pushState({'pageState': current_section}, current_section);
+
         Render();
     });
 
     Render();
-
-  
   });
 
   function Render(){
-
     $('.content_item').hide();
     $("#"+current_section).show();
-    history.pushState({'pageState': current_section}, current_section);
-
-
   }
 
   window.addEventListener('popstate', (event) => {
